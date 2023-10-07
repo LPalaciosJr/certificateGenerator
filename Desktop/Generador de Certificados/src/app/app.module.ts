2e996541360d960e932987c0ca2e6b96e0debdc2
Luis Palacios <lpalacios1119@gmail.com>
2023-10-07 10:41:46 -0600

    commit de publicacion

---
+++ Desktop/Generador de Certificados/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ScanQrComponent } from './components/scan-qr/scan-qr.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ScanQrComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Desktop/Generador de Certificados/src/app/components/scan-qr/scan-qr.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-scan-qr',
+  templateUrl: './scan-qr.component.html',
+  styleUrls: ['./scan-qr.component.css']
+})
+export class ScanQrComponent {
+
+}
